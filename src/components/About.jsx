e8063740de667abc61fa0ad2122be39b0a37dcec
Deividas Jasas <167405264+DeividasJas@users.noreply.github.com>
2024-07-04 21:20:15 +0300

    Second commit, added README file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,15 @@
 function About() {
   return (
     <>
-      <p>
-          My first short project using API that was not made by me
+      <p className='text-lg max-w-lg mx-auto text-slate-300 border border-slate-500 px-2 py-1 rounded-md shadow-lg'>
+        This is my first short project using an external API. It’s nothing too
+        serious, but I enjoyed making it. There's plenty of room for
+        improvement, but that's okay—I have many years ahead to keep learning
+        and growing.
+        <br />
+        Cheers,
+        <br />
+        Jasas
       </p>
     </>
   );
